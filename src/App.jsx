5e47dcdcd1b3a245cc280065fc28a201fd8cdd77
Mauricio <Mauriciogamboa00@gmail.com>
2025-04-05 13:20:18 -0400

    Adding back default React content to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react'; // Importing React to create a React component
-
-import Greetings from './Components/Greetings'; // Importing the Greetings component from the Components folder
-
-// Creating a new component called App
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import Greeting from './Components/Greetings' ; // Importing the Greetings component
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to My first React App</h1>
-            <Greetings />
-        </div>
-    );
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <Greeting />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
-export default App; // Exporting the App component
-  
+
+export default App
